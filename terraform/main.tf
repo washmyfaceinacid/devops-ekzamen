4373972d120b9273cc28bd6ecb7f02a22a4939f6
washmyfaceinacid <superxman402@gmail.com>
2026-04-05 16:17:25 +0300

    terraform infra + cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,7 +132,6 @@ resource "aws_instance" "node" {
   subnet_id                   = aws_subnet.public.id
   vpc_security_group_ids      = [aws_security_group.main.id]
   associate_public_ip_address = true
-  key_name                    = var.key_name != "" ? var.key_name : null
 
   tags = {
     Name = "vasylyshyn-node"
